ac0b89a8a1d839d28a54be7a7ac7516690996645
AntoineReibel <124444455+AntoineReibel@users.noreply.github.com>
2023-05-29 23:50:11 +0200

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  theme: {
+    extend: {
+      
+    },
+    colors: {
+      primary: {
+        pale: 'hsl(225, 100%, 94%)',
+        bright: 'hsl(245, 75%, 52%)',
+      },
+      neutral: {
+        pale: 'hsl(225, 100%, 98%)',
+        desaturate: 'hsl(224, 23%, 55%)',
+        dark: 'hsl(223, 47%, 23%)',
+      },
+      white: '#fff'
+    },
+  },
+  // Autres configurations...
+};
